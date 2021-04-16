cdb3cb049168b3c71883b6c906dc56a0ccb2090c
AnHongjun001 <2637310873@qq.com>
2021-04-16 22:49:49 +0800

    demo

--- demo.cpp
+++ demo.cpp
@@ -11,6 +11,31 @@ bigint jc(int n){
 }
 
 int main(){
+	//initialize a big-number to zero
+	bigint c;	//0
+	//initialize a big-number with a standard integer
+	bigint a(12345);
+	bigint b = 144556643;
+	//operation between big-numbers
+	c = a + b;
+	c.print();
+	c = a - b;
+	c.print();
+	(b-a).print();
+	(a*b).print();
+	//operation between big-number and standard integer
+	(a + 1234).print();
+	(b - 3322).print();
+	(a * (-5)).print();
+	//operation of power
+	a.pow(5).print();	//O(log n)
+	//input a big-number from keyboard
+	read(a);
+	a.print();
+
+	b.read();
+	b.print();
+	//calculate factorial(100)
 	jc(100).print(' ');
 	return 0;
 }
